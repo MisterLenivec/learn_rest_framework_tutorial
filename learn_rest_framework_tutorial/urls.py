995f33dc91cd35717ca2e055eaae47c3daadad81
lenivec <wormsom@gmail.com>
2020-04-30 20:17:10 +0300

    Authentication & Permissions

--- learn_rest_framework_tutorial/urls.py
+++ learn_rest_framework_tutorial/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import include
 
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
